b77f522eb90195d326e9622b4f404a93b8f5bcb5
muzamil hussain <muzamilhussain30072004@gmail.com>
2025-07-14 15:25:14 +0500

    Added edit button

--- reduxtookkittodo/src/components/Todos.jsx
+++ reduxtookkittodo/src/components/Todos.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import {removeTodo} from '../features/todo/todoSlice'
+import {removeTodo , updateTodo} from '../features/todo/todoSlice'
+
 
 function Todos() {
     const todos = useSelector(state => state.todos)
     const dispatch = useDispatch()
+
+    const [editId,setEditId] = useState(null);
+    const [editText,setEditText] = useState('');
 
   return (
     <>
@@ -14,8 +18,56 @@ function Todos() {
     className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
     key={todo.id}
   >
+    <div className='text-white'>{editId === todo.id ? (
+
+      <>
+
+      <input
+      value={editText}
+      onChange={(e) => setEditText(e.target.value)}
+      className='bg-gray-700 text-white px-2 py-1 rounded w-full mr-2'
+      />
+
+      <button
+        onClick={()=>{
+        dispatch(updateTodo({id: editId, newText: editText}));
+        setEditId(null);
+        setEditText('');
+      }}
+       className='bg-green-500 text-white px-3 py-1 rounded ml-2'
+      >
+        update
+      </button>
+
+      </> 
+    ) : (
+    <>
     <div className='text-white'>{todo.text}</div>
-    <button
+    <div>
+
+      <button
+      onClick={()=>{
+        setEditId(todo.id);
+        setEditText(todo.text);
+      }}
+      className='bg-yellow-500 text-white px-3 py-1 rounded'
+      >
+        edit
+      </button>
+      <button
+      onClick={()=> dispatch(removeTodo(todo.id))}
+      className='bg-red-500 text-white px-3 py-1 rounded'
+      >
+        Delete
+        </button>    
+
+    </div>
+    </> 
+    )}
+    
+    
+    </div>
+    {/* <button
       onClick={() => dispatch(removeTodo(todo.id))}
       className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
     >
@@ -33,7 +85,7 @@ function Todos() {
       d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
         />
       </svg>
-    </button>
+    </button> */}
   </li>
 ))}
       </ul>
--- reduxtookkittodo/src/features/todo/todoSlice.js
+++ reduxtookkittodo/src/features/todo/todoSlice.js
@@ -18,9 +18,18 @@ export const todoSlice = createSlice({
         removeTodo: (state,action)=>{
             state.todos = state.todos.filter((todo)=> todo.id !== action.payload)
         },
+
+        updateTodo: (state,action)=>{
+            const {id , newText} = action.payload;
+            const todo = state.todos.find(todo => todo.id === id);
+            if (todo) {
+                todo.text = newText;
+            }
+        }
+
     }
 })
 
-export const {addTodo, removeTodo} = todoSlice.actions
+export const {addTodo, removeTodo , updateTodo} = todoSlice.actions
 
 export default todoSlice.reducer
